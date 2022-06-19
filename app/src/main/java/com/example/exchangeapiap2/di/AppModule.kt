e7b1e7361f7489ad20f7fd4e1f5ebf54075a35fc
Junior Baez <junior_baez@ucne.edu.do>
2022-06-18 21:46:41 -0400

    Implementando app Module

---
+++ app/src/main/java/com/example/exchangeapiap2/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.exchangeapiap2.di
+
+import com.example.exchangeapiap2.ExchangeApi
+import com.example.exchangeapiap2.ExchangeRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinApi(moshi: Moshi): ExchangeApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.coinpaprika.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ExchangeApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(coinApi: ExchangeApi): ExchangeRepository {
+        return ExchangeRepository(coinApi)
+    }
+}
