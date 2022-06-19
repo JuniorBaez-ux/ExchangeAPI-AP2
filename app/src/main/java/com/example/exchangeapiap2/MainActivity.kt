ced115b39619282049f2e6e4f15cbd626cc6bd71
Junior Baez <junior_baez@ucne.edu.do>
2022-06-18 21:54:35 -0400

    Implementando Item para el exchange

--- app/src/main/java/com/example/exchangeapiap2/MainActivity.kt
+++ app/src/main/java/com/example/exchangeapiap2/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.exchangeapiap2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.exchangeapiap2.ui.theme.ExchangeApiAp2Theme
@@ -43,6 +52,43 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+@Composable
+fun ExchangeItem(
+    exchange:ExchangeDto,
+    onClick : (ExchangeDto) -> Unit
+) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable { onClick(exchange) }
+        .padding(16.dp)
+    ) {
+        Text(
+            text = exchange.name,
+            style = MaterialTheme.typography.subtitle1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = exchange.description,
+            style = MaterialTheme.typography.body1,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Text(
+            text = if(exchange.is_active) "Activa" else "Inactiva",
+            color = if(exchange.is_active) Color.Green else Color.Red ,
+            fontStyle = FontStyle.Italic,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+
+        Text(
+            text = exchange.last_updated,
+            style = MaterialTheme.typography.overline,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+
 }
 
 //RUTA: data/remote/dto
